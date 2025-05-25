1fe3adfffcbf1addec58e9f65e9d4bbefb4e244e
Kaushal_26 <kaushalsolanki260102@gmail.com>
2025-05-25 23:00:57 +0530

    feat: Add nullability annotations to request payloads in UrlController

--- src/main/java/io/github/kaushal_26/url_shortener/controller/UrlController.java
+++ src/main/java/io/github/kaushal_26/url_shortener/controller/UrlController.java
@@ -6,6 +6,7 @@ import io.github.kaushal_26.url_shortener.dto.UpdateShortUrlRequest;
 import io.github.kaushal_26.url_shortener.response.*;
 import io.github.kaushal_26.url_shortener.service.UrlService;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,38 +25,38 @@ public class UrlController {
     }
 
     @PostMapping
-    public ResponseEntity<ShortUrlResponse> createShortUrl(@RequestBody CreateShortUrlRequest payload) {
+    public ResponseEntity<ShortUrlResponse> createShortUrl(@NotNull @RequestBody CreateShortUrlRequest payload) {
         log.info("Creating short URL for: {}", payload.getOriginalUrl());
         return new ResponseEntity<>(urlService.createShortUrl(payload.getOriginalUrl()), HttpStatus.CREATED);
     }
 
     @PutMapping
-    public ResponseEntity<ShortUrlResponse> updateShortUrl(@RequestBody UpdateShortUrlRequest payload) {
+    public ResponseEntity<ShortUrlResponse> updateShortUrl(@NotNull @RequestBody UpdateShortUrlRequest payload) {
         log.info("Updating short URL: {} to new original URL: {}", payload.getShortUrl(), payload.getNewOriginalUrl());
         return ResponseEntity.ok(urlService.updateShortUrl(payload.getShortUrl(), payload.getNewOriginalUrl()));
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteShortUrl(@RequestBody ShortUrlRequest payload) {
+    public ResponseEntity<Void> deleteShortUrl(@NotNull @RequestBody ShortUrlRequest payload) {
         log.info("Deleting short URL: {}", payload.getShortUrl());
         urlService.deleteShortUrl(payload.getShortUrl());
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/access-count")
-    public ResponseEntity<GetAccessCountResponse> getAccessCount(@RequestBody ShortUrlRequest payload) {
+    public ResponseEntity<GetAccessCountResponse> getAccessCount(@NotNull @RequestBody ShortUrlRequest payload) {
         log.info("Getting access count for short URL: {}", payload.getShortUrl());
         return ResponseEntity.ok(urlService.getAccessCount(payload.getShortUrl()));
     }
 
     @GetMapping("/last-accessed")
-    public ResponseEntity<GetLastAccessedResponse> getLastAccessedAt(@RequestBody ShortUrlRequest payload) {
+    public ResponseEntity<GetLastAccessedResponse> getLastAccessedAt(@NotNull @RequestBody ShortUrlRequest payload) {
         log.info("Getting last accessed time for short URL: {}", payload.getShortUrl());
         return ResponseEntity.ok(urlService.getLastAccessedAt(payload.getShortUrl()));
     }
 
     @GetMapping("/access-details")
-    public ResponseEntity<GetAccessDetailsResponse> getAccessDetails(@RequestBody ShortUrlRequest payload) {
+    public ResponseEntity<GetAccessDetailsResponse> getAccessDetails(@NotNull @RequestBody ShortUrlRequest payload) {
         log.info("Getting access details for short URL: {}", payload.getShortUrl());
         return ResponseEntity.ok(urlService.getAccessDetails(payload.getShortUrl()));
     }
